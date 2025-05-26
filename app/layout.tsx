8b4a7b96f3c6203ac9526930a65f00582fd25981
221fa04732 <221fa04732@gmail.com>
2025-05-26 17:40:44 +0530

    module not found bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/Providers";
-import Footer from "@/app/components/Footer";
-import Header from "@/app/components/Header";
+import Footer from "./components/footer";
+import Header from "@/app/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
