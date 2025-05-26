fdf50b2826967a13487b799d8d3d896b58fde9a5
221fa04732 <221fa04732@gmail.com>
2025-05-26 11:05:35 +0530

    cleark auth added

--- Providers.tsx
+++ Providers.tsx
@@ -1,11 +1,37 @@
 "use client"
 
 import { RecoilRoot } from "recoil";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 export default function Providers({children} : {
     children : React.ReactNode
 }){
     return(<RecoilRoot >
-        {children}
-    </RecoilRoot>)
-}
+        <ClerkProvider>
+            <header className="h-16 w-full flex justify-center">
+                <div className="flex justify-between items-center w-11/12">
+                    <img src="./logo.webp" alt="logo" />
+                    {/* header tab routing  -- todo  */}
+                    <div className="flex gap-4">
+                        {/* light dark mode -- todo */}
+                        <SignedOut>
+                            <SignInButton />
+                            <SignUpButton />
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </div>
+                </div>
+            </header>
+            {children}
+        </ClerkProvider>
+    </RecoilRoot>
+)}
