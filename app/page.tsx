4885d824c72198c28be84289f396690749504e4e
221fa04732 <221fa04732@gmail.com>
2025-06-11 04:53:24 +0530

    faq and list of company added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { CompanyCard } from "@/components/company"
+import FAQ from "@/components/faq"
 import Landing from "@/components/landing"
 
 export default function Home(){
     return(<div>
         <Landing />
+        <CompanyCard />
+        <FAQ />
     </div>)
 }
---
+++ components/company.tsx
@@ -0,0 +1,20 @@
+import { InfiniteMovingCards } from "./ui/movingcard";
+
+export function CompanyCard() {
+  return (<div className="w-full flex items-center justify-center bg-slate-950 pt-16">
+        <div className="flex md:flex-row flex-col justify-center items-center  text-white w-10/12 md:w-8/12">
+            <div className="text-xl font-semibold min-w-32 flex justify-start items-center">Backed by</div>
+            <div className="flex flex-col antialiased items-center justify-center relative overflow-hidden">
+                <InfiniteMovingCards items={image} direction="left" speed="fast"/>
+            </div>
+        </div>
+    </div>);
+}
+
+const image : string[] =[
+    "./ycombinator.webp",
+    "./calm.webp",
+    "./maiora.webp",
+    "./a16z.webp",
+    "./rebel.webp"
+];
